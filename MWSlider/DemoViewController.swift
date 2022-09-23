69b0ac646fe0efb356141039a040df6404dba811
Wooram Choi <wr@hu-mart.com>
2022-09-23 11:11:34 +0900

    initial commit

---
+++ MWSlider/DemoViewController.swift
@@ -0,0 +1,78 @@
+//
+//  DemoViewController.swift
+//  MWSlider
+//
+//  Created by 최우람 on 2022/09/21.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import Then
+
+class DemoViewController: UIViewController {
+
+    
+    let imageSlider = MWSlider().then {
+        $0.setTitle(title: "image Slider")
+        $0.setImage(urlString: "https://thumbs.dreamstime.com/b/empty-straight-road-top-aerial-view-highway-marking-seamless-roadway-horizontal-template-isolated-white-background-233113314.jpg")
+        $0.maximumValue = 100
+        $0.value = 50
+    }
+    
+    let lottieSlider = MWSlider().then {
+        $0.setTitle(title: "lottie Slider")
+        $0.setLottieImage(urlString: "https://assets2.lottiefiles.com/packages/lf20_fs64xxd6.json")
+        $0.maximumValue = 500
+        $0.value = 350
+    }
+    
+    let gradationSlider = MWSlider().then {
+        $0.setTitle(title: "gradation Slider")
+        $0.setGradient(colors: [UIColor(hex: 0x38D790).cgColor, UIColor(hex: 0x4A79F1).cgColor])
+        $0.maximumValue = 10
+        $0.value = 7
+    }
+    
+    var stackView: UIStackView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.initView()
+    }
+    
+    private func initView() {
+        self.view.backgroundColor = .systemGray5
+        
+        self.stackView = UIStackView().then {
+            $0.axis = .vertical
+            $0.alignment = .fill
+            $0.distribution = .equalSpacing
+            $0.spacing = 48
+            $0.addArrangedSubview(self.imageSlider)
+            $0.addArrangedSubview(self.lottieSlider)
+            $0.addArrangedSubview(self.gradationSlider)
+        }
+        
+        self.view.addSubview(
+            self.stackView
+        )
+        
+        self.makeViewConstraints()
+        
+    }
+    
+    private func makeViewConstraints() {
+        self.stackView.snp.makeConstraints { target in
+            target.leading.trailing.equalToSuperview().inset(24)
+            target.center.equalToSuperview()
+        }
+    }
+
+
+}
+
+
+
---
+++ MWSlider/MWSlider/MWSlider.swift
@@ -0,0 +1,240 @@
+//
+//  MWSlider.swift
+//  MWSlider
+//
+//  Created by 최우람 on 2022/08/30.
+//
+
+import UIKit
+import Lottie
+import SnapKit
+import SDWebImage
+import RxSwift
+import RxCocoa
+
+class MWSlider: UISlider {
+    private var type: BackgroundType = .none
+    
+    var lottieUrlString: String?
+    var imageUrlString: String?
+    var gradientColors: [CGColor]?
+    var height: CGFloat = 56
+    var cornerRadius: CGFloat = 12
+    var borderWidth: CGFloat = 1.0
+    var borderColor = UIColor.init(white: 1.0, alpha: 0.1).cgColor
+    
+    private var bgImage = UIImageView()
+    private var bgLottieView = AnimationView()
+    private var valueLabel = UILabel()
+    private var titleLabel = UILabel()
+    
+    weak var slideTrackDelegate: SlideTrackDelegate?
+    var slideTrackRelay = PublishRelay<Void>()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.commonInit()
+    }
+    
+    override func updateConstraints() {
+        self.snp.makeConstraints { make in
+            make.height.greaterThanOrEqualTo(self.height)
+        }
+        
+        self.bgLottieView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview().inset(-2)
+            make.leading.trailing.equalToSuperview().inset(-3)
+        }
+        
+        self.bgImage.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview().inset(-2)
+            make.leading.trailing.equalToSuperview().inset(-3)
+        }
+        
+        self.titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(8)
+            make.centerY.equalToSuperview()
+        }
+        
+        self.valueLabel.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(16)
+            make.centerY.equalToSuperview()
+        }
+        
+        super.updateConstraints()
+    }
+    private func commonInit() {
+        self.layer.cornerRadius = self.cornerRadius
+        self.layer.borderWidth = self.borderWidth
+        self.layer.borderColor = self.borderColor
+        self.clipsToBounds = true
+        
+        self.setThumbImage(UIImage(), for: .normal)
+        self.setThumbImage(UIImage(), for: .highlighted)
+        
+        self.addSubview(bgImage)
+        self.sendSubviewToBack(bgImage)
+        self.addSubview(self.bgLottieView)
+        self.sendSubviewToBack(self.bgLottieView)
+        
+        self.valueLabel.font = UIFont.boldSystemFont(ofSize: 12)
+        self.valueLabel.textColor = .white
+        self.valueLabel.textAlignment = .right
+        self.valueLabel.sizeToFit()
+        self.addSubview(self.valueLabel)
+        
+        self.titleLabel.font = UIFont.boldSystemFont(ofSize: 12)
+        self.titleLabel.textColor = .white
+        self.titleLabel.textAlignment = .left
+        
+        self.addSubview(self.titleLabel)
+        addTapGesture()
+    }
+    
+    public func addTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        pan.cancelsTouchesInView = false
+        addGestureRecognizer(tap)
+        addGestureRecognizer(pan)
+    }
+    
+    @objc private func handleTap(_ sender: UIGestureRecognizer) {
+        let location = sender.location(in: self)
+        let percent = minimumValue + Float(location.x / bounds.width) * maximumValue
+        setValue(percent, animated: sender.isKind(of: UITapGestureRecognizer.self))
+        sendActions(for: .valueChanged)
+        if sender.state == .ended {
+            self.slideTrackDelegate?.endSlideTracking()
+            self.slideTrackRelay.accept(())
+        }
+    }
+    
+    func setLottieImage(urlString: String) {
+        self.type = .lottie
+        self.lottieUrlString = urlString
+        self.imageUrlString = nil
+    }
+    
+    func setImage(urlString: String) {
+        self.type = .image
+        self.imageUrlString = urlString
+        self.lottieUrlString = nil
+    }
+    
+    func setGradient(colors: [CGColor]) {
+        self.type = .gradient
+        self.gradientColors = colors
+    }
+    
+    func setTitle(title: String) {
+        self.titleLabel.text = title
+        self.titleLabel.sizeToFit()
+    }
+    
+    override func trackRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: 4, y: 0, width: bounds.size.width, height: self.height + 2)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        if type == .lottie, let lottieName = self.lottieUrlString, let url = URL(string: lottieName) {
+            self.bgImage.isHidden = true
+            
+            Animation.loadedFrom(url: url, closure: { (animation) in
+                if let animation = animation {
+                    self.bgLottieView.animation = animation
+                    self.bgLottieView.loopMode = .loop
+                    self.bgLottieView.contentMode = .scaleToFill
+                    self.bgLottieView.backgroundBehavior = .pauseAndRestore
+                    self.bgLottieView.play()
+                }
+                else {
+                    self.drawDefault()
+                }
+            }, animationCache: nil)
+            self.bgLottieView.isUserInteractionEnabled = false
+            self.addSubview(self.bgLottieView)
+            self.sendSubviewToBack(self.bgLottieView)
+        }
+        else if type == .image, let imageName = self.imageUrlString, let url = URL(string: imageName) {
+            self.bgImage.isHidden = false
+            self.bgLottieView.isHidden = false
+            self.bgImage.sd_setImage(with: url, completed: {(image, error, cache, url) in
+                if error != nil {
+                    self.drawDefault()
+                }
+            })
+        }
+        else if type == .gradient, let colors = gradientColors {
+            self.bgLottieView.isHidden = true
+            self.bgImage.isHidden = false
+            let gradient: CAGradientLayer = CAGradientLayer()
+            gradient.colors = colors
+            gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+            gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+            gradient.frame = self.bgImage.bounds
+            self.bgImage.layer.insertSublayer(gradient, at: 0)
+        }
+        
+        self.setMinimumTrackImage(UIColor.init(hex: 0x000000, alpha: 0).toImage(), for: .normal)
+        self.setMaximumTrackImage(UIColor.init(white: 0.0, alpha: type == .gradient ? 1.0 : 0.8).toImage(), for: .normal)
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.valueLabel.text = "\(Int(value))"
+        self.bringSubviewToFront(self.valueLabel)
+        self.bringSubviewToFront(self.titleLabel)
+    }
+    
+    private func drawDefault() {
+        self.type = .gradient
+        self.bgLottieView.isHidden = true
+        self.bgImage.isHidden = false
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [UIColor(hex: 0x38D790).cgColor, UIColor(hex: 0x4A79F1).cgColor]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+        gradient.frame = self.bgImage.bounds
+        self.bgImage.layer.insertSublayer(gradient, at: 0)
+    }
+    
+}
+
+
+enum BackgroundType {
+    case image, lottie, gradient, none
+}
+
+protocol SlideTrackDelegate: AnyObject {
+    func endSlideTracking()
+}
+
+
+// MARK: UIColor Extension
+
+extension UIColor {
+    convenience init(hex: UInt, alpha: CGFloat = 1) {
+        self.init(
+            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(hex & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+    
+    func toImage(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { rendererContext in
+            self.setFill()
+            rendererContext.fill(CGRect(origin: .zero, size: size))
+        }
+    }
+}
